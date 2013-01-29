6e3034f1d01cfaedb66d4211ae26db8dd65126d3
Dominic Cleal <dcleal@redhat.com>
2013-01-29 08:48:48 +0000

    Add code coverage report

--- Gemfile
+++ Gemfile
@@ -7,4 +7,5 @@ gem 'ruby-augeas'
 group :test do
   gem 'puppet'
   gem 'rake'
+  gem 'simplecov'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/spec/"
+end
+
 require 'rspec-puppet-augeas'
 
 RSpec.configure do |c|
